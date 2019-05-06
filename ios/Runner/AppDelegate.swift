45cd5fee9835753816c0368485a34809afd99e4a
LukeyBeachBoy <lb580@kent.ac.uk>
2019-05-06 22:09:22 +0100

    Added platform specific code

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,29 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
   ) -> Bool {
+
+    let controller = window?.rootViewController as! FlutterViewController
+
+    let channel = FlutterMethodChannel(name: "battery", binaryMessenger: controller)
+    channel.setMethodCallHandler({
+      (call:FlutterMethodCall, result: FlutterResult) -> Void in
+      guard call.method == "getBatteryLevel" else {
+        result(FlutterMethodNotImplemented)
+        return
+      }
+      self.receiveBatteryLevel(result: result)
+    })
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  private func receiveBatteryLevel(result: FlutterResult) {
+    let device = UIDevice.current
+    device.isBatteryMonitoringEnabled = true
+    if device.batteryState == UIDeviceBatteryState.unknown {
+      result(FlutterError(code: "Unavailable", message: "Battery info unavailable", details: nil))
+    } else {
+      result(Int(device.batteryLevel * 100))
+    }
   }
 }
