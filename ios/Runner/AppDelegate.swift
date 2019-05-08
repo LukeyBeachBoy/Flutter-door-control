e571cfabb4bddc8e0e7b9de74d5f498ae7083dfd
Luke Beach <lukebeach@Lukes-Mac.local>
2019-05-09 02:05:57 +0530

    Begun work on mqtt connection

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 import LocalAuthentication
 import Flutter
+import MQTTClient
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,10 +10,29 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
   ) -> Bool {
 
-    let controller = window?.rootViewController as! FlutterViewController
+   
     
-    let channel = FlutterMethodChannel(name: "battery", binaryMessenger: controller)
-    channel.setMethodCallHandler({
+    let controller = window?.rootViewController as! FlutterViewController
+    // MQTT CHANNEL SETUP
+    let mqttChannel = FlutterMethodChannel(name: "mqtt", binaryMessenger: controller)
+    mqttChannel.setMethodCallHandler({(call: FlutterMethodCall, result: FlutterResult) -> Void in
+        let method = call.method
+        switch method {
+        case "connect":
+            print("Connecting")
+            self.mqttConnect()
+        case "open":
+            print("Opening")
+        case "close":
+            print("Closing")
+        default:
+            print("Not a valid MQTT command")
+        }
+    })
+   
+    // BATTERY CHANNEL SETUP
+    let batteryChannel = FlutterMethodChannel(name: "battery", binaryMessenger: controller)
+    batteryChannel.setMethodCallHandler({
       (call:FlutterMethodCall, result: FlutterResult) -> Void in
       guard call.method == "getBatteryLevel" else {
         result(FlutterMethodNotImplemented)
@@ -24,7 +44,7 @@ import Flutter
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-
+    // BATTERY FUNCTIONS
   private func receiveBatteryLevel(result: FlutterResult) {
     let device = UIDevice.current
     device.isBatteryMonitoringEnabled = true
@@ -35,4 +55,25 @@ import Flutter
       result(Int(device.batteryLevel * 100))
     }
   }
+    // MQTT FUNCTIONS
+    private func mqttConnect() {
+        let MQTT_HOST = "m24.cloudmqtt.com"
+        let MQTT_PORT: UInt32 = 12432
+        let transport = MQTTCFSocketTransport()
+        let session = MQTTSession()
+        let completion: (()->())?
+        
+        
+        transport.host = MQTT_HOST
+        transport.port = MQTT_PORT
+        session?.transport = transport
+        
+        session?.connect() { error in
+            print("Connection completed with status \(String(describing: error))")
+            if error != nil {
+                
+            }
+        }
+        
+    }
 }
