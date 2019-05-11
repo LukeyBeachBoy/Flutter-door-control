66cad296d3627c08461fd965ac3734e35f52dd40
LukeyBeachBoy <lb580@kent.ac.uk>
2019-05-11 17:07:03 +0100

    Success! Used the mqtt dart plugin by flutter team to communicate with cloudMQTT broker, now going to install to iphone and get the mbed to work

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
-import 'package:flutter/scheduler.dart';
+import 'package:my_app/models/message.dart';
+import 'package:mqtt_client/mqtt_client.dart' as mqtt;
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -25,7 +27,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.red,
       ),
-      home: MyHomePage(title: 'My amazing app'),
+      home: MyHomePage(title: 'Door Control System'),
     );
   }
 }
@@ -49,22 +51,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  MyHomePage myHomePage;
+  String server = 'm24.cloudmqtt.com';
+  int port = 12432;
+  String user = 'dprfbxja';
+  String password = 'dprfbxja';
+  String topic = 'Door';
+  Set<String> topics = Set<String>();
+  List<Message> messages = <Message>[];
+  StreamSubscription subscription;
+  mqtt.MqttClient client;
+  mqtt.MqttConnectionState connectionState;
 
-  int _counter = 0;
-  String mqttStatus = 'Not connected';
   String _batteryPercentage = 'Battery percentage';
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-      print(_counter);
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -74,6 +72,27 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+    IconData connectionStateIcon;
+    switch (client?.connectionState) {
+      case mqtt.MqttConnectionState.connected:
+        connectionStateIcon = Icons.cloud_done;
+        break;
+      case mqtt.MqttConnectionState.disconnected:
+        connectionStateIcon = Icons.cloud_off;
+        break;
+      case mqtt.MqttConnectionState.connecting:
+        connectionStateIcon = Icons.cloud_upload;
+        break;
+      case mqtt.MqttConnectionState.disconnecting:
+        connectionStateIcon = Icons.cloud_download;
+        break;
+      case mqtt.MqttConnectionState.faulted:
+        connectionStateIcon = Icons.error;
+        break;
+      default:
+        connectionStateIcon = Icons.cloud_off;
+    }
+
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -92,74 +111,189 @@ class _MyHomePageState extends State<MyHomePage> {
           new Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                Text(mqttStatus),
+                Icon(
+                  connectionStateIcon,
+                  size: 100,
+                ),
                 MaterialButton(
-                  onPressed: connect,
-                  color: Colors.blueAccent,
+                  minWidth: 250,
+                  color: Colors.redAccent,
+                  onPressed: () {
+                    if (client?.connectionState ==
+                        mqtt.MqttConnectionState.connected) {
+                      _disconnect();
+                    } else
+                      _connect();
+                  },
                   child: Padding(
                     padding: const EdgeInsets.all(16),
                     child: Text(
-                      'Click me',
+                      client?.connectionState ==
+                              mqtt.MqttConnectionState.connected
+                          ? 'Disconnect'
+                          : 'Connect',
                       style: TextStyle(color: Colors.white, fontSize: 35),
                     ),
                   ),
                 ),
-                Text(_batteryPercentage),
-                MaterialButton(
-                  onPressed: _getBatteryInformation,
-                  color: Colors.blueAccent,
-                  child: Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: Text(
-                      'Click me',
-                      style: TextStyle(color: Colors.white, fontSize: 35),
-                    ),
-                  ),
-                ),
-                Text(
-                  'You have pushed the button this many times:',
-                ),
-                Text(
-                  '$_counter',
-                  style: Theme.of(context).textTheme.display1,
-                ),
+                Container(
+                    margin: EdgeInsets.all(20),
+                    child: Column(
+                      children: <Widget>[
+                        MaterialButton(
+                          onPressed: _unlockDoor,
+                          minWidth: 250,
+                          // onPressed: _getBatteryInformation,
+                          color: Colors.greenAccent,
+                          child: Padding(
+                            padding: const EdgeInsets.all(16),
+                            child: Text(
+                              'Unlock',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 35),
+                            ),
+                          ),
+                        ),
+                        MaterialButton(
+                          onPressed: _lockDoor,
+                          minWidth: 250,
+                          // onPressed: _getBatteryInformation,
+                          color: Colors.deepOrangeAccent,
+                          child: Padding(
+                            padding: const EdgeInsets.all(16),
+                            child: Text(
+                              'Lock',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 35),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ))
               ],
             ),
           )
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        elevation: 15,
-        backgroundColor: Color.fromRGBO(229, 64, 64, 100),
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
-  static const mqttChannel = const MethodChannel('mqtt');
-  Future<void> connect() async {
+  Future _connect() async {
+    client = mqtt.MqttClient.withPort(server, '', port);
+    client.onDisconnected = _onDisconnected;
+
+    final mqtt.MqttConnectMessage connMess = mqtt.MqttConnectMessage()
+        .authenticateAs('dprfbxja', '_3rLfEpgyHqs')
+        .withClientIdentifier('Mqtt_MyClientUniqueId2')
+        // Must agree with the keep alive set above or not set
+        .startClean() // Non persistent session for testing
+        .keepAliveFor(600)
+        // If you set this you must set a will message
+        .withWillTopic('Door')
+        .withWillMessage('Mobile connected')
+        .withWillQos(mqtt.MqttQos.atLeastOnce);
+    print('MQTT client connecting....');
+    client.connectionMessage = connMess;
+
     try {
-      var res = await mqttChannel.invokeMethod('connect');
-    } on PlatformException catch (e) {
-      print('Error connecting');
+      await client.connect();
+    } catch (e) {
+      print(e);
+      _disconnect();
     }
+
+    if (client.connectionState == mqtt.MqttConnectionState.connected) {
+      print('MQTT client connected');
+      setState(() {
+        connectionState = client.connectionState;
+        _subscribeToTopic('Door');
+      });
+    } else {
+      print('ERROR: MQTT client connection failed - '
+          'disconnecting, state is ${client.connectionState}');
+      _disconnect();
+    }
+    subscription = client.updates.listen(_onMessage);
   }
 
-  static const batteryChannel = const MethodChannel('battery');
-  Future<void> _getBatteryInformation() async {
-    String batteryPercentage;
-    try {
-      var res = await batteryChannel.invokeMethod('getBatteryLevel');
-      batteryPercentage = 'Battery level at $res%';
-    } on PlatformException catch (e) {
-      batteryPercentage = "Failed to get battery level: '${e.message}'.";
-    }
+  void _disconnect() {
+    client.disconnect();
+    _onDisconnected();
+  }
 
+  void _onDisconnected() {
     setState(() {
-      _batteryPercentage = batteryPercentage;
+      topics.clear();
+      connectionState = client.connectionState;
+      client = null;
+      subscription.cancel();
+      subscription = null;
+    });
+    print('MQTT client disconnected');
+  }
+
+  void _onMessage(List<mqtt.MqttReceivedMessage> event) {
+    print(event.length);
+    final mqtt.MqttPublishMessage recMess =
+        event[0].payload as mqtt.MqttPublishMessage;
+    final String message =
+        mqtt.MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+
+    /// The above may seem a little convoluted for users only interested in the
+    /// payload, some users however may be interested in the received publish message,
+    /// lets not constrain ourselves yet until the package has been in the wild
+    /// for a while.
+    /// The payload is a byte buffer, this will be specific to the topic
+    print('MQTT message: topic is <${event[0].topic}>, '
+        'payload is <-- ${message} -->');
+    print(client.connectionState);
+    setState(() {
+      messages.add(Message(
+        topic: event[0].topic,
+        message: message,
+        qos: recMess.payload.header.qos,
+      ));
     });
   }
+
+  void _subscribeToTopic(String topic) {
+    if (connectionState == mqtt.MqttConnectionState.connected) {
+      setState(() {
+        if (topics.add(topic.trim())) {
+          print('Subscribing to ${topic.trim()}');
+          client.subscribe(topic, mqtt.MqttQos.exactlyOnce);
+        }
+      });
+    }
+  }
+
+  void _unlockDoor() {
+    final mqtt.MqttClientPayloadBuilder builder =
+        mqtt.MqttClientPayloadBuilder();
+    builder.addString('unlock');
+    client.publishMessage('Door', mqtt.MqttQos.exactlyOnce, builder.payload);
+  }
+
+  void _lockDoor() {
+    final mqtt.MqttClientPayloadBuilder builder =
+        mqtt.MqttClientPayloadBuilder();
+    builder.addString('lock');
+    client.publishMessage('Door', mqtt.MqttQos.exactlyOnce, builder.payload);
+  }
+  // static const batteryChannel = const MethodChannel('battery');
+  // Future<void> _getBatteryInformation() async {
+  //   String batteryPercentage;
+  //   try {
+  //     var res = await batteryChannel.invokeMethod('getBatteryLevel');
+  //     batteryPercentage = 'Battery level at $res%';
+  //   } on PlatformException catch (e) {
+  //     batteryPercentage = "Failed to get battery level: '${e.message}'.";
+  //   }
+  //   setState(() {
+  //     _batteryPercentage = batteryPercentage;
+  //   });
+  // }
 }
